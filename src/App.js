b55ecf5e66c3febabb3096adaf34a4b75f27432a
Morgan Li <morganli135@gmail.com>
2024-03-20 19:11:12 -0600

    add cat pic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import catPicture from './cat.jpg'; // Importing cat picture
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={catPicture} className="App-logo" alt="cat" /> {/* Using cat picture */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
